fdc064b05f324cb4ba1fd76e8109680b6dfb5b42
Birgit Sutzl <bss116@ic.ac.uk>
2020-04-09 11:27:05 +0200

    remove canyon checks which are not properly working

--- citygenerator/checks.py
+++ citygenerator/checks.py
@@ -1,9 +1,7 @@
 import numpy as np
-import operator
-import citygenerator as cgen
 
 
-def heightratios(blocks, domainheight):
+def get_heightratios(blocks, domainheight):
     """Function that calculates building height to domain height ratio. 
     General advice: domain height/building height >= 6."""
     ratios = []
@@ -19,7 +17,7 @@ def heightratios(blocks, domainheight):
     return ratios
 
 
-def blockvolumes(blocks):
+def get_blockvolumes(blocks):
     """Function that calculates the cube roots of the block volume."""
     roots = []
     for block in blocks:
@@ -30,24 +28,11 @@ def blockvolumes(blocks):
     return roots
 
 
-def canyonwidths(fullcanyons):
-    """Function that calculates the canyon widths."""
-    xwidths = []
-    ywidths = []
-    for canyon in fullcanyons:
-        width1 = (canyon[1] - canyon[0])
-        width2 = (canyon[3] - canyon[2])
-        xwidths.append(width1)
-        ywidths.append(width2)
-    
-    return xwidths, ywidths
-
-
 def check_heightratio(dimblocks, domainheight, heightratio=6):
     # blocks and limits are dimensionalised
     problemblocks = []
     # check for domain height zsize over block height zmax ratio:
-    ratios = heightratios(dimblocks, domainheight)
+    ratios = get_heightratios(dimblocks, domainheight)
     
     for r, block in zip(ratios, dimblocks):
         if r < heightratio:
@@ -61,7 +46,7 @@ def check_blockvolume(blocks, blockvolume=10):
     # blocks are in cells, not dimensionalised
     problemblocks = []
     # check for block volume:
-    roots = blockvolumes(blocks)
+    roots = get_blockvolumes(blocks)
     for r, block in zip(roots, blocks):
         if r < blockvolume:
             problemblocks.append(block)
@@ -70,34 +55,9 @@ def check_blockvolume(blocks, blockvolume=10):
     return problemblocks
 
 
-def check_canyonwidths(blocks, limits, canyonwidth=5):
-    # blocks are dimensionalised, therefore we should set higher width
-    problemcanyons = []
-    # get canyons
-    canyons, fullcanyons = cgen.canyons.getcanyons(blocks, limits)
-    # get canyon widths
-    xwidths, ywidths = canyonwidths(fullcanyons)
-
-    # get canyon shift
-    initblocks = sorted(blocks, key=operator.itemgetter(2,0))
-    xshift = initblocks[0][0]  # first block xmin
-    yshift = initblocks[0][2]  # first block ymin
-    
-    # check canyon widths
-    for xwid, ywid, fullcanyon in zip(xwidths, ywidths, fullcanyons):
-        if xwid < canyonwidth or ywid < canyonwidth:
-            # shift back
-            canyon = cgen.utils.blockshift([fullcanyon], limits, -xshift, -yshift)
-            problemcanyons.extend(canyon)
-            print("Smaller canyon width with widths = ", xwid, "and", ywid, "for canyon ", canyon, ".")
-
-    return problemcanyons
-
-
-def allchecks(blocks, limits, resolution, heightratio=6, blockvolume=10, canyonwidth=5):
+def run_checks(blocks, limits, resolution, heightratio=6, blockvolume=10):
         
     problemblocks = []
-    problemcanyons = []
 
     try:
         probblocks1 = check_heightratio(blocks, limits[5], heightratio)
@@ -116,15 +76,6 @@ def allchecks(blocks, limits, resolution, heightratio=6, blockvolume=10, canyonw
         print("Could not check block volume.")
         pass
         
-#     try:
-#         # CANYONS WILL NOT ALWAYS BE WELL DEFINED
-#         probcanyons = check_canyonwidths(blocks, limits[:4], canyonwidth)
-#         problemcanyons.extend(probcanyons)
-#     except Exception:
-#         print("Could not check canyon widths.")
-#         pass  
-            
     print("Checks completed.")
     
-    return problemblocks, problemcanyons
-    
+    return problemblocks
